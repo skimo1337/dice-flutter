c3a2839438c6b9b49a571a7f012de208aad022e7
Katji Afrjal <53615916+skimo1337@users.noreply.github.com>
2021-06-10 03:35:56 +0100

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'dart:ui';
+import 'dart:math';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Center(child: Text('Dicee')),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int left = 1;
+  int right = 1;
+  void change() {
+    setState(() {
+      left = Random().nextInt(6) + 1;
+      right = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                change();
+              },
+              child: Image.asset('images/dice$left.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+                onPressed: () {
+                  change();
+                },
+                child: Image.asset('images/dice$right.png')),
+          ),
+        ],
+      ),
+    );
+  }
+}
